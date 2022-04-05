3f6c646c4e7a268f27f2cb6b7d8d02d002e8679a
Kaushal000 <Kaushalsengupta007@protonmail.com>
2022-04-05 15:40:12 +0530

    Update Dockerfiles

--- src/app.py
+++ src/app.py
@@ -147,11 +147,11 @@ def detect_objects(our_image,score_threshold,nms_threshold):
         
     with col1:
         st.header("Original image")
-        st.image(our_image,use_column_width='never')
+        st.image(our_image,use_column_width='always')
 
     with col2:
         st.header("Detected objects in the image")
-        st.image(img,use_column_width='never')
+        st.image(img,use_column_width='always')
 
         
         
