4208c4006a204057928c8809f9ca009469d78c37
Kaushal000 <Kaushalsengupta007@protonmail.com>
2022-04-05 12:20:37 +0530

    Minor improvements

--- src/video.py
+++ src/video.py
@@ -14,16 +14,17 @@ def getvideo(name):
     writer=None
 
   
-    if 'key' not in st.session_state:
-        st.session_state.key = 'Detecting confidences'
+    # if 'key' not in st.session_state:
+    #     st.session_state.key = 'Detecting confidences'
     
     class_labels = ["corona_virus"]
     parentdir=os.path.dirname(__file__)
     childdir='videos'
     videopath=os.path.join(parentdir,childdir)
     if not os.path.exists(videopath):
-        mode=0o777
-        os.mkdir(videopath,mode)
+        with st.spinner("Creating directory to store resultant video"):
+            mode=0o777
+            os.mkdir(videopath,mode)
     vid=os.path.join(videopath,'result.webm')
 
 
@@ -60,6 +61,10 @@ def getvideo(name):
 
     stinfo = st.empty()
     stframe = st.empty()
+    
+    with st.spinner("Detecting objects from the frames of the uploaded video, labeling frames by drawing bounding boxes around detected objects and writing the labelled frames to the resultant video"):
+        time.sleep(5)
+    
     while vf.isOpened():
         stf=st.empty()
         ret, frame = vf.read()
@@ -81,117 +86,117 @@ def getvideo(name):
         # Calculating at the same time, needed time for forward pass
         network.setInput(blob)  # setting blob as input to the network
         
-        with st.spinner("Detecting from frames,drawing bounding boxes around the detected objects and writing down the individual labelled frames to video"):
-            start = time.time()
-            output_from_network = network.forward(layers_names_output)
-            end = time.time()
+     
+        start = time.time()
+        output_from_network = network.forward(layers_names_output)
+        end = time.time()
 
-            # Increasing counters for frames and total time
-            f += 1
-            t += end - start
+        # Increasing counters for frames and total time
+        f += 1
+        t += end - start
 
-            # Showing spent time for single current frame
-            # print('Frame number {0} took {1:.5f} seconds'.format(f, end - start))
-            stinfo.info('Frame number {0} took {1:.5f} seconds'.format(f, end - start))
-            
-            class_ids_list = []
-            boxes_list = []
-            confidences_list = []
-            
-            
-            for object_detection_layer in output_from_network :
-            # loop over the detections
-                for object_detection in object_detection_layer:
+        # Showing spent time for single current frame
+        # print('Frame number {0} took {1:.5f} seconds'.format(f, end - start))
+        stinfo.info('Frame number {0} took {1:.5f} seconds'.format(f, end - start))
+        
+        class_ids_list = []
+        boxes_list = []
+        confidences_list = []
+        
+        
+        for object_detection_layer in output_from_network :
+        # loop over the detections
+            for object_detection in object_detection_layer:
+                
+                # obj_detections[1 to 4] => will have the two center points, box width and box height
+                # obj_detections[5] => will have scores for all objects within bounding box
+                all_scores = object_detection[5:]
+                predicted_class_id = np.argmax(all_scores)
+                prediction_confidence = all_scores[predicted_class_id]
+
+                # take only predictions with confidence more than 20%
+                if prediction_confidence > 0.20:
+                    #get the predicted label
+                    predicted_class_label = class_labels[predicted_class_id]
+                    #obtain the bounding box co-oridnates for actual image from resized image size
+                    bounding_box = object_detection[0:4] * np.array([img_width, img_height, img_width, img_height])
+                    (box_center_x_pt, box_center_y_pt, box_width, box_height) = bounding_box.astype("int")
+                    start_x_pt = int(box_center_x_pt - (box_width / 2))
+                    start_y_pt = int(box_center_y_pt - (box_height / 2))
                     
-                    # obj_detections[1 to 4] => will have the two center points, box width and box height
-                    # obj_detections[5] => will have scores for all objects within bounding box
-                    all_scores = object_detection[5:]
-                    predicted_class_id = np.argmax(all_scores)
-                    prediction_confidence = all_scores[predicted_class_id]
-
-                    # take only predictions with confidence more than 20%
-                    if prediction_confidence > 0.20:
-                        #get the predicted label
-                        predicted_class_label = class_labels[predicted_class_id]
-                        #obtain the bounding box co-oridnates for actual image from resized image size
-                        bounding_box = object_detection[0:4] * np.array([img_width, img_height, img_width, img_height])
-                        (box_center_x_pt, box_center_y_pt, box_width, box_height) = bounding_box.astype("int")
-                        start_x_pt = int(box_center_x_pt - (box_width / 2))
-                        start_y_pt = int(box_center_y_pt - (box_height / 2))
-                        
-                        ############## NMS Change 2 ###############
-                        #save class id, start x, y, width & height, confidences in a list for nms processing
-                        #make sure to pass confidence as float and width and height as integers
-                        class_ids_list.append(predicted_class_id)
-                        confidences_list.append(float(prediction_confidence))
-                        boxes_list.append([start_x_pt, start_y_pt, int(box_width), int(box_height)])
-                        ############## NMS Change 2 END ###########
-                    
-            ############## NMS Change 3 ###############
-            # Applying the NMS will return only the selected max value ids while suppressing the non maximum (weak) overlapping bounding boxes      
-            # Non-Maxima Suppression confidence set as 0.5 & max_suppression threhold for NMS as 0.4 (adjust and try for better perfomance)
-            max_value_ids = cv2.dnn.NMSBoxes(boxes_list, confidences_list,0.5,0.4)
+                    ############## NMS Change 2 ###############
+                    #save class id, start x, y, width & height, confidences in a list for nms processing
+                    #make sure to pass confidence as float and width and height as integers
+                    class_ids_list.append(predicted_class_id)
+                    confidences_list.append(float(prediction_confidence))
+                    boxes_list.append([start_x_pt, start_y_pt, int(box_width), int(box_height)])
+                    ############## NMS Change 2 END ###########
+                
+        ############## NMS Change 3 ###############
+        # Applying the NMS will return only the selected max value ids while suppressing the non maximum (weak) overlapping bounding boxes      
+        # Non-Maxima Suppression confidence set as 0.5 & max_suppression threhold for NMS as 0.4 (adjust and try for better perfomance)
+        max_value_ids = cv2.dnn.NMSBoxes(boxes_list, confidences_list,0.5,0.4)
+        
+        # loop through the final set of detections remaining after NMS and draw bounding box and write text
+        for max_valueid in max_value_ids:
+            max_class_id = max_valueid
+            box = boxes_list[max_class_id]
+            start_x_pt = box[0]
+            start_y_pt = box[1]
+            box_width = box[2]
+            box_height = box[3]
             
-            # loop through the final set of detections remaining after NMS and draw bounding box and write text
-            for max_valueid in max_value_ids:
-                max_class_id = max_valueid
-                box = boxes_list[max_class_id]
-                start_x_pt = box[0]
-                start_y_pt = box[1]
-                box_width = box[2]
-                box_height = box[3]
+            #get the predicted class id and label
+            predicted_class_id = class_ids_list[max_class_id]
+            predicted_class_label = class_labels[predicted_class_id]
+            prediction_confidence = confidences_list[max_class_id]
+        ############## NMS Change 3 END ########### 
                 
-                #get the predicted class id and label
-                predicted_class_id = class_ids_list[max_class_id]
-                predicted_class_label = class_labels[predicted_class_id]
-                prediction_confidence = confidences_list[max_class_id]
-            ############## NMS Change 3 END ########### 
-                    
-                end_x_pt = start_x_pt + box_width
-                end_y_pt = start_y_pt + box_height
-                
-                #get a random mask color from the numpy array of colors
-                box_color = colors[predicted_class_id]
-                
-                #convert the color numpy array as a list and apply to text and box
-                box_color = [int(c) for c in box_color]
-                
-                # print the prediction in console
-                predicted_class_label = "{}: {:.2f}%".format(predicted_class_label, prediction_confidence * 100)
-                
-                
-                # draw rectangle and text in the image
-                cv2.rectangle(frame, (start_x_pt, start_y_pt), (end_x_pt, end_y_pt), box_color, 1)
-                cv2.putText(frame, predicted_class_label, (start_x_pt, start_y_pt-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, box_color, 2)
-                # Preparing lists for detected bounding boxes,
-                # obtained confidences and class's number
+            end_x_pt = start_x_pt + box_width
+            end_y_pt = start_y_pt + box_height
             
-                
-                #checkpoint 
-                #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            #get a random mask color from the numpy array of colors
+            box_color = colors[predicted_class_id]
             
-                    
-            stframe.image(frame,use_column_width='yes')
-
-            if writer is None:
-            # Constructing code of the codec
-            # to be used in the function VideoWriter
-                # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                fourcc = cv2.VideoWriter_fourcc('V','P','8','0')
+            #convert the color numpy array as a list and apply to text and box
+            box_color = [int(c) for c in box_color]
+            
+            # print the prediction in console
+            predicted_class_label = "{}: {:.2f}%".format(predicted_class_label, prediction_confidence * 100)
+            
+            
+            # draw rectangle and text in the image
+            cv2.rectangle(frame, (start_x_pt, start_y_pt), (end_x_pt, end_y_pt), box_color, 1)
+            cv2.putText(frame, predicted_class_label, (start_x_pt, start_y_pt-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, box_color, 2)
+            # Preparing lists for detected bounding boxes,
+            # obtained confidences and class's number
+        
+            
+            #checkpoint 
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
                 
-                # fourcc = 0x00000021
-                # fourcc = cv2.VideoWriter_fourcc(*'vp80')
+        stframe.image(frame,use_column_width='yes')
 
-                # Writing current processed frame into the video file
-                # Pay attention! If you're using Windows, yours path might looks like:
-                # r'videos\result-traffic-cars.mp4'
-                # or:
-                # 'videos\\result-traffic-cars.mp4'
-                writer = cv2.VideoWriter(vid, fourcc, 20,(frame.shape[1], frame.shape[0]), True)
+        if writer is None:
+        # Constructing code of the codec
+        # to be used in the function VideoWriter
+            # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            fourcc = cv2.VideoWriter_fourcc('V','P','8','0')
+            
+            # fourcc = 0x00000021
+            # fourcc = cv2.VideoWriter_fourcc(*'vp80')
 
-        # Write processed current frame to the file
-            writer.write(frame)
-            # stf.success("predicted object {}".format(predicted_class_label))    
+            # Writing current processed frame into the video file
+            # Pay attention! If you're using Windows, yours path might looks like:
+            # r'videos\result-traffic-cars.mp4'
+            # or:
+            # 'videos\\result-traffic-cars.mp4'
+            writer = cv2.VideoWriter(vid, fourcc, 30,(frame.shape[1], frame.shape[0]), True)
+
+    # Write processed current frame to the file
+        writer.write(frame)
+        # stf.success("predicted object {}".format(predicted_class_label))    
         
     if not ret:
         stinfo.info("End of video. Writing the detected frames and showing the resultant video...")
@@ -204,7 +209,6 @@ def getvideo(name):
             stframe.empty()        
         
         # videopath=os.path.join(os.path.dirname( __file__ ),'video','result.mp4')
-        print(os.listdir(videopath))
         video_file=open(vid,'rb')
         video_bytes = video_file.read()
         st.video(video_bytes)
